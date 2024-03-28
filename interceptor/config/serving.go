3378588585bc4e1a6827369ee71f78e1f220ba28
Isala Piyarisi <mail@isala.me>
2024-03-28 20:37:19 +0530

    Add request queue cooldown (#16)
    * Revert "Revert "Add request queue cooldown (#14)""
    
    This reverts commit 028369b1ca3a1f46101d2ec5875ba2b9727c77d5.
    
    * Fix deadlock issue in ProcessPostponedResizes function

--- interceptor/config/serving.go
+++ interceptor/config/serving.go
@@ -26,6 +26,13 @@ type Serving struct {
 	//
 	// This is the interval (in milliseconds) representing how often to do a fetch
 	DeploymentCachePollIntervalMS int `envconfig:"KEDA_HTTP_DEPLOYMENT_CACHE_POLLING_INTERVAL_MS" default:"250"`
+
+	// This indicates how long the interceptor should wait before setting the queue size to 0
+	// This prevents from KEDA asuming the service is not in use
+	RequestQueueCooldown time.Duration `envconfig:"KEDA_HTTP_REQUEST_QUEUE_COOLDOWN" default:"20s"`
+
+	// This is the interval at which the interceptor will check if the queue size should be set to 0
+	RequestQueueCooldownEnforcerInterval time.Duration `envconfig:"KEDA_HTTP_REQUEST_QUEUE_COOLDOWN_ENFORCER_INTERVAL" default:"5s"`
 }
 
 // Parse parses standard configs using envconfig and returns a pointer to the
