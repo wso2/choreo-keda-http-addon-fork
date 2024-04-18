9b2521ebeab6ce12cdd2037393db3f1aec5783db
Isala Piyarisi <mail@isala.me>
2024-04-18 11:53:55 +0530

    Fix a bug in retrying and queue postponing (#23)

--- interceptor/forward_wait_func.go
+++ interceptor/forward_wait_func.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
@@ -57,8 +56,6 @@ func newDeployReplicasForwardWaitFunc(
 						"Didn't get a deployment back in event",
 					)
 				} else if deploymentCanServe(*deployment) {
-					// waiting 2s after healthcheck passes
-					time.Sleep(2000)
 					return 0, nil
 				}
 			case <-ctx.Done():
--- interceptor/request_forwarder.go
+++ interceptor/request_forwarder.go
@@ -29,7 +29,6 @@ func forwardRequest(
 		req.URL.RawQuery = r.URL.RawQuery
 		req.Header.Del("X-Forwarded-For")
 	}
-
 	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 		w.WriteHeader(502)
 		errMsg := fmt.Errorf("error on backend (%w)", err).Error()
@@ -40,7 +39,6 @@ func forwardRequest(
 			)
 		}
 	}
-
 	proxy.ModifyResponse = func(resp *http.Response) error {
 		if resp.StatusCode == http.StatusServiceUnavailable {
 			buf := new(bytes.Buffer)
@@ -50,26 +48,39 @@ func forwardRequest(
 			}
 			resp.Body.Close()
 			resp.Body = io.NopCloser(bytes.NewReader(buf.Bytes()))
-
 			// Check if the response body starts with "upstream connect error or disconnect/reset before headers"
 			if bytes.HasPrefix(buf.Bytes(), []byte("upstream connect error or disconnect/reset before headers")) {
-				retries := 0
-				for retries < maxRetries {
-					lggr.Info("Service unavailable, retrying", "attempt", retries+1)
-					time.Sleep(time.Second * time.Duration(2*(retries+1)))
-
-					if resp, err = http.DefaultTransport.RoundTrip(resp.Request); err == nil {
-						return nil
-					}
-
-					retries++
-				}
-				lggr.Error(nil, "Max retries reached, returning last response")
-				return nil
+				return retryRequest(lggr, resp.Request, maxRetries, 0)
 			}
 		}
 		return nil
 	}
-
 	proxy.ServeHTTP(w, r)
+}
+
+func retryRequest(lggr logr.Logger, req *http.Request, maxRetries, attempt int) error {
+	if attempt >= maxRetries {
+		lggr.Error(nil, "Max retries reached, returning last response")
+		return nil
+	}
+	lggr.Info("Service unavailable, retrying", "attempt", attempt+1)
+	time.Sleep(time.Second * time.Duration(2*(attempt+1)))
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		return retryRequest(lggr, req, maxRetries, attempt+1)
+	}
+	if resp.StatusCode == http.StatusServiceUnavailable {
+		buf := new(bytes.Buffer)
+		_, err := buf.ReadFrom(resp.Body)
+		if err != nil {
+			return err
+		}
+		resp.Body.Close()
+		resp.Body = io.NopCloser(bytes.NewReader(buf.Bytes()))
+		// Check if the response body starts with "upstream connect error or disconnect/reset before headers"
+		if bytes.HasPrefix(buf.Bytes(), []byte("upstream connect error or disconnect/reset before headers")) {
+			return retryRequest(lggr, req, maxRetries, attempt+1)
+		}
+	}
+	return nil
 }
