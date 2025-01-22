99459b1655d324daeb2374b82effdee2d1f07a1f
Lahiru De Silva <lahirude@wso2.com>
2025-01-22 17:04:53 +0530

    revert port routing from URL and remove unwanted logs
    Signed-off-by: Lahiru De Silva <lahirude@wso2.com>

--- interceptor/middleware/routing.go
+++ interceptor/middleware/routing.go
@@ -45,17 +45,13 @@ func (rm *Routing) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r = util.RequestWithLoggerWithName(r, "RoutingMiddleware")
 	ctx := r.Context()
 	logger := util.LoggerFromContext(ctx)
-	hostHeader := r.Header.Get("Host")
-	logger.Info("Routing", "received host", r.Host, "url", r.URL.String(), "port", r.URL.Port(), "RequestURI", r.RequestURI, "hostHeader", hostHeader)
 	host, err := getHost(r, rm.reverseDNSRetry, rm.reverseDNSRetryInterval)
-	logger.Info("Routing", "host", r.Host, "modifiedHost", host)
 	if err != nil {
 		sh := handler.NewStatic(http.StatusNotFound, err)
 		sh.ServeHTTP(w, r)
 	}
 	r.Host = host
 	httpso := rm.routingTable.Route(r)
-	logger.Info("Routing", "httpso", httpso)
 	if httpso == nil {
 		if rm.isProbe(r) {
 			rm.probeHandler.ServeHTTP(w, r)
